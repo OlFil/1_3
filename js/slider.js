fa113972c57d07a9a233322ea53d464b12843aab
OlFil <miss-olenka2000@yandex.ru>
2022-12-03 03:21:37 +0400

    1_5

---
+++ js/slider.js
@@ -0,0 +1,31 @@
+function ready() {
+    let slider__toggle = true;
+    let slider__toggleButton = document.querySelector('.slider__read-more-btn');
+    let slider__list = document.querySelector('.slider__brands');
+    slider__list.style.height = '160px';
+    slider__list.style.overflow = 'hidden';
+
+    let img = document.createElement("img");
+    img.classList.add('read-more-btn__icon');
+
+
+    slider__toggleButton.addEventListener("click", () => {
+
+        if (slider__toggle) {
+            slider__list.style.height = 'auto';
+            slider__toggleButton.innerHTML = 'Скрыть';
+            img.src = "../assets/svg/icon.svg";
+            slider__toggleButton.prepend(img)
+        } else {
+            slider__list.style.height = '160px';
+            slider__list.style.overflow = 'hidden';
+            slider__toggleButton.innerHTML = 'Показать все';
+            img.src = "../assets/svg/more.svg";
+            slider__toggleButton.prepend(img)
+        }
+
+        slider__toggle = !slider__toggle;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", ready);
